649dfef8e7dd902260e03d1517f373501518f31c
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-06 12:09:13 -0300

    Atualização do desafio079.py

--- desafio079.py
+++ desafio079.py
@@ -1,3 +1,24 @@
-#valores = []
-valores = [23, 79, 22, 33, 40, 79]
-teste
+valores = []
+i = 0
+c = ""
+ordem = []
+While True:
+    append.valores(int(input("Digite um número: "))
+    while not valores[i].isnumeric:
+        append.valores(int(input("Valor inválido! Por favor, digite um número: "))
+    while valores[i] in valores[i]: #nao repete  valor inserido dentro da lista 
+        del(valores[i])
+    c = str(input("Você quer adicionar mais algum número? [S/N]: ")).strip().upper()[0]
+    while c not in "NS":
+        c = str(input("Você quer adicionar mais algum número? [S/N]: ")).strip().upper()[0]
+    while "NS" in c:
+        if "N" in c:
+            break
+print(f"Você digitou os números: ", end=" ")
+for v in valores:
+    print(v, end=" ")
+ordem = valores.sort()
+print(f"\nOrdem crescente dos números: ",)
+for o in ordem:
+    print(o, end=" ")
+print("Programa encerrado")
