990100d85e6b0b2d3a9f300f765e7a9790b9d45d
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-06 15:37:21 -0300

    Resolvendo desafio080.py

--- desafio080.py
+++ desafio080.py
@@ -1,2 +1,15 @@
-#n = []
-n = [3, 10, 21, 19, 34]
+numeros = []
+for i in range(0, 5):
+    numeros.append(int(input("Digite um número: "))) #Digita 5 números
+    if len(numeros) == 1:
+        numeros.append(int(input("Digite um número: ")))
+    elif len(numeros) >= 2:
+        a = numeros[i]
+        b = numeros[i-1]
+        if a > b:
+            numeros.append(a)
+        elif b > a:
+            numeros.remove(b)
+            numeros.insert(b, a)
+        del(a,b)
+    print(numeros)
