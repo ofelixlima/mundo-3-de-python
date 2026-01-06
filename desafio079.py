fe7dbc4723e51562d7da19062afa03de6e6e51e9
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-06 12:43:12 -0300

    Atualizar o desafio079.py

--- desafio079.py
+++ desafio079.py
@@ -1,19 +1,21 @@
-valores = []
+valores = ordem = []
 i = 0
 c = ""
-ordem = []
-While True:
-    append.valores(int(input("Digite um número: "))
+while True:
+    append.valores(str(input("Digite um número: ").strip()[i])
     while not valores[i].isnumeric:
-        append.valores(int(input("Valor inválido! Por favor, digite um número: "))
+        append.valores(str(input("Valor inválido! Digite um número: ").strip()[i])
+    valores[i] = int(valores[i])
     while valores[i] in valores[i]: #nao repete  valor inserido dentro da lista 
         del(valores[i])
+    
     c = str(input("Você quer adicionar mais algum número? [S/N]: ")).strip().upper()[0]
     while c not in "NS":
         c = str(input("Você quer adicionar mais algum número? [S/N]: ")).strip().upper()[0]
     while "NS" in c:
         if "N" in c:
             break
+    i += 1
 print(f"Você digitou os números: ", end=" ")
 for v in valores:
     print(v, end=" ")
