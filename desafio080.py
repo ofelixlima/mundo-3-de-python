3e0a6f056f6fc5b40a2944811e44d90b7b0654f4
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-06 17:49:31 -0300

    Desafio080 em resolução

--- desafio080.py
+++ desafio080.py
@@ -1,17 +1,20 @@
-numeros = []
-for i in range(0, 5):
-    numeros.append(int(input("Digite um número: "))) #5
-    if len(numeros) >= 2: #12
-        b = numeros[i] #12
-        a = numeros[i-1] #5
-        if a > b:
-            numeros.remove(a)
-            numeros.insert(b, a)
-        elif b > a:
-            numeros.remove(b)
-            numeros.insert(a, b)
-        del(b, a)
-        if len(numeros) >= 3:
-            for j in numeros:
-                if j[i] < j[i]
-    print(numeros) #[12, 12, 12, 12, 90, 12]
+ordem = numeros = []
+#numeros = [12, 2, 4, 10, 45]
+for i in range(0,5):
+    numeros.append(int(input("Digite um número: ")))
+        #Se o número digitado é maior que o digitado anteriormente
+    print(f"Números digitados: {numeros}")
+    if len(numeros) > 1 and numeros[i] > numeros[i-1]: #Se a lista contém mais de um número e o número atual é maior do que o número anterior
+    print("Sim")
+    
+    """if len(numeros) != 0: #Se a lista números não estiver vazia
+        a = max(numeros) #Encontre o maior número
+        ordem.append(a) #Adicione-o na lista ordem
+        if a in numeros: #Se o maior número estiver na lista numeros
+            numeros.remove(a) #Remova-o
+            print(f"Restaram: {numeros}") #Mostre os números que restaram
+    elif len(numeros) == 0: #Se a lista numeros ficar vazia
+        print(f"Lista ordenada: ", end="")
+        for o in ordem: #Me mostre a lista ordem
+            print(f"{o}", end=" ")
+        break"""
