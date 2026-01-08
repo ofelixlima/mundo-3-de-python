bb5a625813ea78166c112ae7ccc555ac9e51134b
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-08 00:30:16 -0300

    Desafio080 resolvi com ajuda de IA na lógica

--- desafio080.py
+++ desafio080.py
@@ -1,21 +1,11 @@
-n = [12, 14, 110, 21, 2, 56, 0, 78]
-ultimo = anterior = []
-for i in range(0,8):
-    for j in range(0,8):
-        #print(f"n[{i}], n[{j}]: {n[i], n[j]}")
-        anterior = n[i]
-        ultimo = n[j]
-        if anterior == ultimo:
-            anterior < ultimo
-        elif len(n) > 1:
-            if anterior > ultimo:
-                temp = anterior
-                n.remove(anterior)
-                n.append(temp)
-                del temp
-            elif anterior < ultimo:
-                temp = ultimo
-                n.remove(ultimo)
-                n.append(temp)
-                del temp
-        print(n)
+n = tam = []
+for i in range(0,5):
+    n.append(int(input("Digite um número: ")))
+    tam = len(n)
+    p = len(n)-1
+    while p > 0 and n[p] < n[p-1]:
+        temp = n[p]
+        n[p] = n[p-1]
+        n[p-1] = temp
+        p = p -1
+    print(n)
