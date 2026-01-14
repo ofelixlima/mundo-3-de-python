a6dd20b3e0358a973e535be0af201330dd6e1c53
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-14 11:53:27 -0300

    Desafio089.py resolvido sem dificuldade

--- desafio089.py
+++ desafio089.py
@@ -1,3 +1,4 @@
+from time import sleep
 temp = []
 notas = []
 alunos = []
@@ -10,6 +11,7 @@ while True:
     notas.clear()
     media = sum(temp[1])/len(temp[1])
     temp.append(media)
+    del media
     alunos.append(temp[:])
     temp.clear()
     #print(alunos) #[['Marcos', [6, 9], 7.5], ['Felix', [7, 9], 8.0]]
@@ -17,5 +19,25 @@ while True:
     while continuar not in "SN":
         continuar = str(input("Quer adicionar mais um aluno? [S/N]: ")).strip().upper()[0]
     if continuar in "N":
+        nome = [n[0] for n in alunos]
+        notas = [p[1] for p in alunos]
+        media = [m[2] for m in alunos]
+        print("=-"*50)
+        print(f"{"Nº":<6} {"NOME":<10} {"MÉDIA":<5}")
+        print("-"*25)
+        for i in range(0,len(nome)):
+            print(f"{i:<6} {nome[i]:<10} {media[i]:<5}")
+        print("-"*50)
+        escolha = 0
+        while escolha != 999:
+            escolha = int(input("Deseja mostrar as notas de qual aluno? (999 interrompe): "))
+            while escolha > len(alunos)-1 and escolha < 999:
+                escolha = int(input("Quantidade inválida! Deseja mostrar as notas de qual aluno? (999 interrompe): "))
+            if escolha == 999:
+                break
+            elif escolha != 999 and escolha <= len(alunos)-1:
+                print(f"Notas de {nome[escolha]} são {notas[escolha]}.")
         break
-print("Programa encerrado!")
+print(f"FINALIZANDO...")
+sleep(1)
+print("<<< VOLTE SEMPRE >>>")
--- desafio089temp.py
+++ desafio089temp.py
@@ -1,2 +1,19 @@
 alunos = [['Marcos', [6, 9], 7.5], ['Felix', [7, 9], 8.0]]
-print()
+nome = [n[0] for n in alunos]
+notas = [p[1] for p in alunos]
+media = [m[2] for m in alunos]
+print("=-"*50)
+print(f"{"Nº":<6} {"NOME":<10} {"MÉDIA":<5}")
+print("-"*25)
+for i in range(0,len(nome)):
+    print(f"{i:<6} {nome[i]:<10} {media[i]:<5}")
+print("-"*50)
+escolha = 0
+while escolha != 999:
+    escolha = int(input("Deseja mostrar as notas de qual aluno? (999 interrompe): "))
+    while escolha > len(alunos)-1 and escolha < 999:
+        escolha = int(input("Quantidade inválida! Deseja mostrar as notas de qual aluno? (999 interrompe): "))
+    if escolha == 999:
+        break
+    elif escolha != 999 and escolha <= len(alunos)-1:
+        print(f"Notas de {nome[escolha]} são {notas[escolha]}.")
