50145eeea3cffc9f5bdb17a7385e427437ce4de1
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-15 12:19:47 -0300

    Inclusão da aula019.py completa - partindo para os desafios...

--- aula019.py
+++ aula019.py
@@ -1,4 +1,4 @@
-pessoas = {"nome": "Gustavo", "sexo": "M", "idade": 22}
+'''pessoas = {"nome": "Gustavo", "sexo": "M", "idade": 22}
 print(pessoas) #{"nome": "Gustavo", "sexo": "M", "idade": 22}
 print(pessoas['nome']) #Gustavo
 print(pessoas['idade']) #22
@@ -27,4 +27,120 @@ for k, v in pessoas.items():
     sexo = M
     idade = 22
     """
-#20:15
+"""
+#Apagando o sexo do dicionário
+del pessoas['sexo']
+for k, v in pessoas.items():
+    print(f"{k} = {v}")
+    
+    #nome = Gustavo
+    #idade = 22
+"""
+"""
+#Modificando info existentes
+pessoas['nome'] = "Leandro"
+for k, v in pessoas.items():
+    print(f"{k} = {v}")
+    #nome = Leandro
+    #sexo = M
+    #idade = 22
+"""
+"""
+#Adicionando elementos no dicionário
+pessoas['peso'] = 98.5
+for k, v in pessoas.items():
+    print(f"{k} = {v}")
+    #nome = Gustavo
+    #sexo = M
+    #idade = 22
+    #peso = 98.5
+"""
+'''
+
+"""
+#Dicionario dentro de lista / Lista com dicionários
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', "sigla": "RJ"}
+estado2 = {'uf': 'São Paulo', "sigla": "SP"}
+brasil.append(estado1)
+brasil.append(estado2)
+#print(estado1) #{'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+#print(estado2) #{'uf': 'São Paulo', "sigla": "SP"}
+#print(brasil) #[{'uf': 'Rio de Janeiro', 'sigla': 'RJ'}, {'uf': 'São Paulo', 'sigla': 'SP'}]
+#print(brasil[0]) #{'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+#print(brasil[1]) #{'uf': 'São Paulo', "sigla": "SP"}
+#print(brasil[0]['uf']) #Rio de Janeiro
+#print(brasil[1]['sigla']) #SP
+
+"""
+"""
+estado = {}
+brasil = []
+for c in range(0,3):
+    estado['uf'] = str(input("Unidade federativa: ")).strip().capitalize()
+    estado['sigla'] = str(input("Sigla de Estado: ")).strip().upper()
+    brasil.append(estado)
+print(brasil) #Deu erro
+#[{'uf': 'Minas gerais', 'sigla': 'MG'}, {'uf': 'Minas gerais', 'sigla': 'MG'}, {'uf': 'Minas gerais', 'sigla': 'MG'}]
+"""
+"""
+estado = {}
+brasil = []
+for c in range(0,3):
+    estado['uf'] = str(input("Unidade federativa: ")).strip().capitalize()
+    estado['sigla'] = str(input("Sigla de Estado: ")).strip().upper()
+    brasil.append(estado[:])
+print(brasil)
+''' Erro esperado
+brasil.append(estado[:])
+              ~~~~~~^^^
+KeyError: slice(None, None, None)'''"""
+"""
+estado = {}
+brasil = []
+for c in range(0,3):
+    estado['uf'] = str(input("Unidade federativa: ")).strip().capitalize()
+    estado['sigla'] = str(input("Sigla de Estado: ")).strip().upper()
+    brasil.append(estado.copy()) #O método copy() copia um elemento do dicionário para a lista.
+#print(brasil) #[{'uf': 'São paulo', 'sigla': 'SP'}, {'uf': 'Rio de janeiro', 'sigla': 'RJ'}, {'uf': 'Espírito santo', 'sigla': 'ES'}]
+for e in brasil:
+    print(e)
+    '''
+    {'uf': 'Rio', 'sigla': 'RJ'}
+    {'uf': 'Sampa', 'sigla': 'SP'}
+    {'uf': 'Paraná', 'sigla': 'PR'}
+    '''
+"""
+"""estado = {}
+brasil = []
+for c in range(0,3):
+    estado['uf'] = str(input("Unidade federativa: ")).strip().capitalize()
+    estado['sigla'] = str(input("Sigla de Estado: ")).strip().upper()
+    brasil.append(estado.copy()) #O método copy() copia um elemento do dicionário para a lista.
+#print(brasil) #[{'uf': 'São paulo', 'sigla': 'SP'}, {'uf': 'Rio de janeiro', 'sigla': 'RJ'}, {'uf': 'Espírito santo', 'sigla': 'ES'}]
+for e in brasil: #for da lista
+    for k, v in e.items(): #chave e valor no e (cada dicionário)
+        print(f"O campo {k} tem valor {v}.")
+'''
+O campo uf tem valor Rio.
+O campo sigla tem valor RJ.
+O campo uf tem valor Sampa.
+O campo sigla tem valor SP.
+O campo uf tem valor Goias.
+O campo sigla tem valor GO.
+'''"""       
+
+estado = {}
+brasil = []
+for c in range(0,3):
+    estado['uf'] = str(input("Unidade federativa: ")).strip().capitalize()
+    estado['sigla'] = str(input("Sigla de Estado: ")).strip().upper()
+    brasil.append(estado.copy()) #O método copy() copia um elemento do dicionário para a lista.
+#print(brasil) #[{'uf': 'São paulo', 'sigla': 'SP'}, {'uf': 'Rio de janeiro', 'sigla': 'RJ'}, {'uf': 'Espírito santo', 'sigla': 'ES'}]
+for e in brasil: #for da lista
+    for v in e.values(): #chave e valor no e (cada dicionário)
+        print(v, end=" ")
+    print()
+#Rio RJ 
+#Sampa SP
+#Bahia BH
