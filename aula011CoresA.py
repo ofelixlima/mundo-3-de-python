2166ad872b61c1f6890481afacdbd2a0453c56d3
Felix Lima <32372236+ofelixlima@users.noreply.github.com>
2026-01-22 15:29:47 -0300

    Commit: Revendo e exercitando a aula de cores para conseguir fazer os desafios seguintes

---
+++ aula011CoresA.py
@@ -0,0 +1,10 @@
+print('\033[31mOlá Mundo!') #Vermelho
+print('\033[1;33mOlá Mundo!') #Amarelo
+print('\033[1;31;43mOlá Mundo!') #Vermelho sobre Amarelo em negrito
+print('\033[1;31;43mOlá Mundo!\033[m') #Vermelho sobre Amarelo em negrito com limite final
+print('\033[4;30;45mOlá Mundo!\033[m') #branco sobre lilás
+print('\033[30mOlá Mundo!\033[m') #branco com preto
+print('\033[7;30mOlá Mundo!\033[m') #preto com branco
+print('\033[0;33;44mOlá Mundo!\033[m') #amarelo sobre azul
+print('\033[7;33;44mOlá Mundo!\033[m') #azul sobre amarelo
+#Commit: Revendo e exercitando a aula de cores para conseguir fazer os desafios seguintes
---
+++ aula011CoresD.py
@@ -0,0 +1,7 @@
+nome = 'Guanabara'
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33m',
+         'preto e branco': '\033[7;30m'}
+#print(f'Olá! Muito prazer em te conhecer, {cores['amarelo']}{nome}{cores['limpa']}!!!')
+print(f'Olá! Muito prazer em te conhecer, {cores['preto e branco']}{nome}{cores['limpa']}!!!')
